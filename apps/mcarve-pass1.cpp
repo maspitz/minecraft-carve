d7149c8366d8ddcf6a56f8e716d5c7bee4252d20
Michael Spitznagel <exultavit@gmail.com>
2023-04-21 00:23:25 -0400

    Scan for timestamp blocks

--- apps/mcarve-pass1.cpp
+++ apps/mcarve-pass1.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <ctime>
 #include <stdexcept>
+#include <cstdint>  // for types uint32_t etc.
+#include <vector>
+
+using namespace std;
 
 time_t parse_time(const std::string& timestr) {
     struct tm tm = {};
@@ -53,6 +57,59 @@ int main(int argc, char *argv[]) {
     	std::cout << "Start Timestamp: " << start_time << "\n"
 		<< "Stop Timestamp:  " << stop_time << std::endl;
     }
+
+    const uint64_t BLOCK_SIZE = 4096;
+
+    ifstream infile(filename, ios::in | ios::binary);
+    if (!infile) {
+        cerr << "Error: Could not open file " << filename << endl;
+        return 1;
+    }
+
+    infile.seekg(0, ios::end);
+    uint64_t file_size = infile.tellg();
+    if (file_size % BLOCK_SIZE != 0) {
+        cerr << "Warning: image file size is not padded to align with block size." << endl;
+    }
+    infile.seekg(0, ios::beg);
+    if (verbose) {
+        cout << "Opened image file " << filename << "\n";
+        cout << "Contains " << file_size / 4096 << " blocks" << endl;
+    }
+
+    constexpr auto FIELD_SIZE = sizeof(uint32_t);
+    constexpr auto FIELDS_PER_BLOCK = BLOCK_SIZE / FIELD_SIZE;
+    uint32_t buffer[FIELDS_PER_BLOCK];
+
+    vector<uint64_t> timestamp_offsets;
+
+    for (uint64_t offset = 0; offset < file_size; offset += BLOCK_SIZE) {
+        infile.seekg(offset, ios::beg);
+        infile.read((char*)buffer, BLOCK_SIZE);
+
+        bool is_timestamp = false;
+        for (uint64_t i = 0; i < FIELDS_PER_BLOCK; ++i) {
+
+            uint32_t timestamp = __builtin_bswap32(buffer[i]);
+            if (timestamp != 0) {
+                is_timestamp = true;
+                if (timestamp < start_time || timestamp > stop_time) {
+                    is_timestamp = false;
+                    break;
+                }
+            }
+        }
+
+        if (is_timestamp) {
+            timestamp_offsets.push_back(offset);
+        }
+    }
+    cout << "Found " << timestamp_offsets.size()
+         << " candidate timestamp blocks at offsets:\n\t";
+    for(auto offset: timestamp_offsets) {
+         cout << offset << " ";
+    }
+    cout << endl;
 
     return 0;
 }
