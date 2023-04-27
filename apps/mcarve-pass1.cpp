5d75107945a6feafb30cbebe5f5342b10c8fb00b
Michael Spitznagel <exultavit@gmail.com>
2023-04-26 23:27:20 -0400

    Refactor offset block test

--- apps/mcarve-pass1.cpp
+++ apps/mcarve-pass1.cpp
@@ -49,6 +49,48 @@ bool is_timestamp_block(const std::vector<unsigned char> &blockdata,
     return is_timestamp;
 }
 
+
+
+bool is_offset_block(const std::vector<unsigned char> &blockdata) {
+    auto uint32data = reinterpret_cast<const uint32_t *>(blockdata.data());
+    const auto n_fields = blockdata.size() / sizeof(uint32_t);
+    for (size_t i = 0; i < n_fields; ++i) {
+        uint32_t x = __builtin_bswap32(uint32data[i]);
+        uint8_t upper_byte = static_cast<uint8_t>((x >> 24) & 0xFF);
+        if (upper_byte & 0xFE) {
+            return false;
+        }
+    }
+
+    map<int32_t, uint8_t> chunk_length;
+    for (size_t i = 0; i < n_fields; ++i) {
+        uint32_t x = __builtin_bswap32(uint32data[i]);
+        uint32_t offset = x >> 8 & 0xFFFFFF;
+        uint8_t length = static_cast<uint8_t>(x & 0xFF);
+        if (length > 0) {
+            chunk_length[offset] = length;
+        }
+    }
+
+    uint32_t calculated_offset{2};
+
+    if (chunk_length.size() < 3) {
+        return false;
+    }
+    int trials = 0;
+    for (auto i : chunk_length) {
+        trials++;
+        uint32_t offset(i.first);
+        auto length = i.second;
+        if (offset < calculated_offset) {
+            return false;
+        }
+        calculated_offset += length;
+    }
+    return true;
+}
+
+
 // bool is_chunk_header(const array<uint32_t, FIELDS_PER_BLOCK> &buffer) {
 //     uint32_t data_length = __builtin_bswap32(buffer[0]);
 //     // Require the chunk data length to be less than 1 MiB.
@@ -97,46 +139,6 @@ using bin64 = bitset<64>;
 //     return x.to_ullong();
 // }
 
-// bool is_offset_block(const array<uint32_t, FIELDS_PER_BLOCK> &buffer) {
-//     for (size_t i = 0; i < FIELDS_PER_BLOCK; ++i) {
-//         uint32_t x = __builtin_bswap32(buffer[i]);
-//         uint8_t upper_byte = static_cast<uint8_t>((x >> 24) & 0xFF);
-//         if (upper_byte & 0xFE) {
-//             return false;
-//         }
-//     }
-
-//     map<int32_t, uint8_t> chunk_length;
-//     for (size_t i = 0; i < FIELDS_PER_BLOCK; ++i) {
-//         uint32_t x = __builtin_bswap32(buffer[i]);
-//         uint32_t offset = x >> 8 & 0xFFFFFF;
-//         uint8_t length = static_cast<uint8_t>(x & 0xFF);
-//         if (length > 0) {
-//             chunk_length[offset] = length;
-//         }
-//     }
-
-//     uint32_t calculated_offset{2};
-
-//     if (chunk_length.size() < 3) {
-//         return false;
-//     }
-//     int trials = 0;
-//     for (auto i : chunk_length) {
-//         trials++;
-//         uint32_t offset(i.first);
-//         auto length = i.second;
-//         if (offset < calculated_offset) {
-//             cerr << "Failed at " << trials << " out of " <<
-//             chunk_length.size()
-//                  << endl;
-//             return false;
-//         }
-//         calculated_offset += length;
-//     }
-//     return true;
-// }
-
 int main(int argc, char *argv[]) {
 
     unique_ptr<Pass1Config> conf_ptr;
@@ -172,6 +174,10 @@ int main(int argc, char *argv[]) {
                           ? "This is "
                           : "This is not ")
                   << "a timestamp block." << std::endl;
+        std::cout << (is_offset_block(data)
+                          ? "This is "
+                          : "This is not ")
+                  << "a offset block." << std::endl;
     }
 
     //     for (uint64_t offset = 0; offset < file_size; offset += BUFFER_SIZE)
