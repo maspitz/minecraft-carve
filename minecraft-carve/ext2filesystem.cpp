862e0ee3cb962b2898ba5418304a1dbe82d5c1e3
Michael Spitznagel <exultavit@gmail.com>
2023-08-14 15:14:11 -0400

    Refactor: Add const qualifiers

--- minecraft-carve/ext2filesystem.cpp
+++ minecraft-carve/ext2filesystem.cpp
@@ -29,17 +29,18 @@ Ext2Filesystem::Ext2Filesystem(const std::string &name) {
 
 Ext2Filesystem::~Ext2Filesystem() { ext2fs_close(m_fs); }
 
-bool Ext2Filesystem::block_is_used(uint64_t blk) {
+bool Ext2Filesystem::block_is_used(uint64_t blk) const {
     return ext2fs_test_block_bitmap2(m_fs->block_map, blk);
 }
 
 void Ext2Filesystem::read_block(uint64_t blk, std::vector<unsigned char> &data,
-                                unsigned count) {
+                                unsigned count) const {
     data = std::vector<unsigned char>(m_fs->blocksize * count);
     read_block(blk, &data[0], count);
 }
 
-void Ext2Filesystem::read_block(uint64_t blk, void *data, unsigned count) {
+void Ext2Filesystem::read_block(uint64_t blk, void *data,
+                                unsigned count) const {
     errcode_t errval = io_channel_read_blk64(m_fs->io, blk, count, data);
     if (errval) {
         com_err("Ext2Filesystem::read_block()", errval, "while reading block");
--- minecraft-carve/ext2filesystem.hpp
+++ minecraft-carve/ext2filesystem.hpp
@@ -13,13 +13,15 @@ class Ext2Filesystem {
     Ext2Filesystem(const std::string &name);
     ~Ext2Filesystem();
 
-    bool block_is_used(uint64_t blk);
+    bool block_is_used(uint64_t blk) const;
     void read_block(uint64_t blk, std::vector<unsigned char> &data,
-                    unsigned count = 1);
-    void read_block(uint64_t blk, void *data, unsigned count = 1);
-    unsigned int blocksize() { return m_fs->blocksize; }
-    uint32_t first_data_block() { return m_fs->super->s_first_data_block; }
-    uint64_t blocks_count() { return ext2fs_blocks_count(m_fs->super); }
+                    unsigned count = 1) const;
+    void read_block(uint64_t blk, void *data, unsigned count = 1) const;
+    unsigned int blocksize() const { return m_fs->blocksize; }
+    uint32_t first_data_block() const {
+        return m_fs->super->s_first_data_block;
+    }
+    uint64_t blocks_count() const { return ext2fs_blocks_count(m_fs->super); }
 
   private:
     ext2_filsys m_fs;
